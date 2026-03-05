737e8aa6423d8bb5a6b3dc3656095a780bd1c407
saiharanadh <saiharanadhreddykaipu@gmail.com>
2026-03-05 15:35:36 +0530

    Use Case 2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,37 @@
-public class BookMyStayApp {
-    public static void main(String args[]) {
-        System.out.println("Welcome to Hotel Booking Management System");
-        System.out.println("System Initialized Successfully");
+public abstract class Room{
+    protected int numberofbeds;
+    protected int squarefeet;
+    protected double pricepernight;
+
+    public Room(int numberofbeds,int squarefeet,double pricepernight){
+        this.numberofbeds=numberofbeds;
+        this.squarefeet=squarefeet;
+        this.pricepernight=pricepernight;
+    }
+    public void displayroomdetails(){
+        System.out.println("numberofbeds:"+numberofbeds);
+        System.out.println("squarefeet:"+squarefeet);
+        System.out.println("pricepernight"+pricepernight);
     }
 }
+public class SingleRoom extends Room{
+    public SingleRoom(){
+        super(1,250,1500.0);
+    }
+}
+public class DoubleRoom extends Room{
+    public DoubleRoom(){
+        super(2,400,2500.0);
+    }
+}
+public class SuiteRoom extends Room{
+    public SuiteRoom(){
+        super(3,750,5000.0);
+    }
+}
+public class BookMyStayApp{
+
+}
+
+
+
